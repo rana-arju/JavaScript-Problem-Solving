87edbadbaeca8573a3c4083a1d430a8a738faadd
rana-arju <arjurana20@gmail.com>
2025-06-12 20:53:34 +0600

    sum of digits

--- common-problem.js
+++ common-problem.js
@@ -1,15 +1,21 @@
 //factorial
-function fact(n){
-  if (n === 0) return 1
-  return n * fact(n-1)
+function fact(n) {
+  if (n === 0) return 1;
+  return n * fact(n - 1);
 }
 
 // fibonacci
 
-
 function fibo(n) {
-  if (n <= 1) return n
-  return fibo(n-1) + fibo(n-2)
+  if (n <= 1) return n;
+  return fibo(n - 1) + fibo(n - 2);
 }
 
-console.log(fibo(6));
+// sum of digits
+function sumOfDigits(n) {
+  return n
+    .toString()
+    .split("")
+    .reduce((sum, num) => sum + parseInt(num), 0);
+}
+console.log(sumOfDigits(1234)); // 6
